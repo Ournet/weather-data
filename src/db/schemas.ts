a5fad62fd195d1be1963df1d92cd8676c5b761f0
Dumitru Cantea <canteadumitru@gmail.com>
2018-02-05 18:36:55 +0200

    v0.1.2

--- src/db/schemas.ts
+++ src/db/schemas.ts
@@ -5,7 +5,7 @@ export const ReportSchema = Joi.object().keys({
     id: Joi.string().trim().min(2).max(50).required(),
     
     units: Joi.string().valid(['si']).required(),
-    data: Joi.string().min(10).max(5000).required(),
+    data: Joi.string().min(10).max(6000).required(),
 
     createdAt: Joi.date().required(),
     expiresAt: Joi.date().required(),
